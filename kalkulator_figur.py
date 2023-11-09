a9d831212f417d02d67d79e05e1e9f2bdf6a3c66
Ventisek <jakub.chlupek@gmail.com>
2023-11-09 10:25:28 +0100

    .

---
+++ kalkulator_figur.py
@@ -0,0 +1,14 @@
+import math 
+
+print("Obliczanie Pol Figur i ich Obwodow")
+print("                     ")
+print("to sa mozliwe figury z ktorych mozesz obliczyc pole i obwod:")
+print("1-Kwadrat 2-Prostokat 3-Trojkat 4-Okrag 5-Rab 6-Deltoid ")
+
+figura=int(input("W takim razie podaj liczbe odpowiadajaca figurze ktorej chcesz obliczyc pole i obwod :"))
+if figura==1:
+    print("W takim razie obliczasz pole kwadratu")
+    a=float(input("Podaj liczbe a: "))
+    print("a oto obwod kwadratu: ")
+    
+    
